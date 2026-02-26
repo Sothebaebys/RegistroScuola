6f2b151a80328e66bb165d90f866c0c07f0665fd
Sothebaebys <176383400+Sothebaebys@users.noreply.github.com>
2026-02-26 18:20:42 +0100

    Funzione per la visualizzazione dei registri

--- Registro/Program.cs
+++ Registro/Program.cs
@@ -1,7 +1,25 @@
 ﻿class Registro
 {
+    static Dictionary<string, Dictionary<string, List<int>>> registroVoti = new Dictionary<string, Dictionary<string, List<int>>>()
+   {
+      {
+        "Mario Rossi",
+        new Dictionary<string, List<int>>
+        {
+            { "Matematica", new List<int> { 7, 8, 6 } },
+            { "Italiano", new List<int> { 6, 7 } }
+        }
+      },
+      {
+         "Luca Bianchi",
+         new Dictionary<string, List<int>>
+         {
+               { "Matematica", new List<int> { 9, 8 } },
+               { "Storia", new List<int> { 7, 7, 8 } }
+         }
+      }
+   };
    // mettiamo le funzioni qui
-
    //Dizionario : { nome : dizionario<materie:listaint> }
    //Accetta come argomento un dizionario che come chiave ha la materia e come valore il voto
    public static void VisualizzaRegistro(bool isSingola)
@@ -19,36 +37,58 @@
             string separatore = "=================================================="; 
 
             Console.WriteLine($"{separatore}\n============REGISTRO============\n{separatore}");
-            Console.WriteLine($"Studente: {registroVoti.Keys()}");
+            Console.WriteLine($"Studente: {nome}\n{separatore}");
 
-            Console.WriteLine($"\nMATERIA || VOTO ||\n{separatore}");
+            Console.WriteLine($"\nMATERIA      || VOTO    ||\n{separatore}");
 
-            int totaleVoti = dizMaterie.Value.Lenght;
+            
 
-            foreach (var materia in dizMaterie)
+            foreach (var materia in registroVoti[nome])
             {
-               Console.WriteLine($"{separatore}\n ");
-               Console.Write($"{materia.Key}: ");  
+                int totaleVoti = registroVoti[nome][materia.Key].Count;
+                Console.WriteLine($"\n{separatore}\n ");
+                Console.Write($"{materia.Key}: ");  
 
-               for (int i = 0; i<totaleVoti; i++)
-                  Console.Write($" {materia.Value[i]}");
-               Console.Write($"||\n{separatore}");
+                for (int i = 0; i<totaleVoti; i++)
+                    Console.Write($" {materia.Value[i]}");
+                Console.Write($"\n{separatore}\n");
 
             }
          }
 
          else
          {
-            Console.WriteLine($"Nome non presente nei registri.");
-            
+            Console.WriteLine($"Nome non presente nei registri.\nRiprova:");
+            VisualizzaRegistro(true);
          }
       
       }  
       else
       {
-         foreach (var studente in dizMaterie)
+         foreach (var studente in registroVoti)
          {
-            //VisualizzaRegistro(dizMaterie,true);
+            string separatore = "=================================================="; 
+
+            Console.WriteLine($"{separatore}\n=====================REGISTRO=====================\n{separatore}");
+            Console.WriteLine($"Studente: {studente.Key}\n{separatore}");
+
+            Console.WriteLine($"\nMATERIA      || VOTO    ||\n{separatore}");
+
+            foreach (var materia in studente.Value)
+            {
+                Console.WriteLine($"\n{separatore}\n ");
+                Console.Write($"{materia.Key}: ");  
+
+                foreach (var voto in materia.Value)
+                {
+                    Console.Write($" {voto}");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine($"\n{separatore}\n ");
+
+
          }
       }
    
@@ -56,7 +96,7 @@
 
    public static void Main(string[] args)
    {
-      VisualizzaRegistro(true);
+      VisualizzaRegistro(false);
    }
 
 }
--- Registro/obj/Debug/net10.0/Registro.AssemblyInfo.cs
+++ Registro/obj/Debug/net10.0/Registro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Registro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc475cb437a3faf6b87847ebcc113ca0b5ac90e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db2cb026fec737eca5ddd0be05deb1cab7c7be3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Registro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Registro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
